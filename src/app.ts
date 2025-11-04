e3bcdd2973a847d9abf369ccf51a17ab41d9a230
Toutvendre <ducvfx07@gmail.com>
2025-11-04 20:43:36 +0000

    premier commit pour le verification 2

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ const app = express();
 
 // Middleware CORS amélioré
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://dipamaweb.netlify.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
